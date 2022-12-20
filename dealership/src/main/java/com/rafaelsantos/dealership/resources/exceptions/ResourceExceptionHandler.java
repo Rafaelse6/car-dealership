d60003fb5756d0d65ce7b65613b169b4bb3553d3
Rafael Santos <rafa.05125@gmail.com>
2022-12-20 20:05:19 -0300

    DELETE Category and Database exception

--- dealership/src/main/java/com/rafaelsantos/dealership/resources/exceptions/ResourceExceptionHandler.java
+++ dealership/src/main/java/com/rafaelsantos/dealership/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.rafaelsantos.dealership.services.exceptions.DatabaseException;
 import com.rafaelsantos.dealership.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,14 +16,30 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest  request){
-	StandardError err = new StandardError();
-	err.setTimeStamp(Instant.now());
-	err.setStatus(HttpStatus.NOT_FOUND.value());
-	err.setError("Resource not found");
-	err.setMessage(e.getMessage());
-	err.setPath(request.getRequestURI());
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError();
+		err.setTimeStamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Resource not found");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
 	
-	return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		
+		StandardError err = new StandardError();
+		err.setTimeStamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
 	}
 }
--- dealership/src/main/java/com/rafaelsantos/dealership/services/CategoryService.java
+++ dealership/src/main/java/com/rafaelsantos/dealership/services/CategoryService.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rafaelsantos.dealership.dto.CategoryDTO;
 import com.rafaelsantos.dealership.entities.Category;
 import com.rafaelsantos.dealership.repositories.CategoryRepository;
+import com.rafaelsantos.dealership.services.exceptions.DatabaseException;
 import com.rafaelsantos.dealership.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -51,6 +53,16 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found");
+		} catch (DatabaseException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
