2ccb9ab04eadd78f767005648cd02e49f28b940e
Rafael Santos <rafa.05125@gmail.com>
2022-12-21 17:50:25 -0300

    Auditing data

--- dealership/src/main/resources/import.sql
+++ dealership/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category(name) VALUES ('SUV');
-INSERT INTO tb_category(name) VALUES ('Hatchback');
-INSERT INTO tb_category(name) VALUES ('Crossover');
+INSERT INTO tb_category(name, created_At) VALUES ('SUV', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Hatchback', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Crossover',NOW());
